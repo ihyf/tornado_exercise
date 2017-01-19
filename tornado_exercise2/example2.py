b3e9afee366d82409fc51770c808538d70bf9f07
ihyf <huang_yingfei@hollywant.com>
2017-01-19 18:29:36 +0800

    session 存进redis

--- tornado_exercise2/example2.py
+++ tornado_exercise2/example2.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
-#-*- coding: utf-8 -*-
+#-*- coding: utf-8 -*
 
 import os
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
+import tornado.autoreload
+from tornado.options import define, options
+from pycket.session import SessionMixin
+
+define('port', default=8888, help='fuck', type=int)
+define('debug', default=False, help='fuck', type=bool)
 
 
 class HomeHandler(tornado.web.RequestHandler):
@@ -13,13 +19,15 @@ class HomeHandler(tornado.web.RequestHandler):
         self.render('index.html')
 
 
-class LoginHandler(tornado.web.RequestHandler):
+class LoginHandler(tornado.web.RequestHandler, SessionMixin):
 
     def get(self, *args, **kwargs):
         self.render('login.html')
 
     def post(self, *args, **kwargs):
+        print self.get_argument('user', None)
         self.set_secure_cookie('user', self.get_argument('user', None))
+        self.session.set('user_session', self.get_argument('user', None))
         self.write('Successfully set cookie!')
 
 
@@ -29,12 +37,13 @@ class OtherHtmlHandler(tornado.web.RequestHandler):
         user = self.get_secure_cookie('user')
         return user
 
+    @tornado.web.authenticated
     def get(self, page):
         #import pdb
         # pdb.set_trace()
-        if not self.current_user:
-            self.redirect("/login.html")
-            return
+        # if not self.current_user:
+        #     self.redirect("/login.html")
+        #     return
         pagename = page + '.html'
         path = os.path.join(self.settings['static_path'], pagename)
         print path
@@ -54,7 +63,7 @@ class OtherHandler(tornado.web.RequestHandler):
 
 class CustomApplication(tornado.web.Application):
 
-    def __init__(self):
+    def __init__(self, debug=False):
         handles = [
             (r'/$', HomeHandler),
             (r'/login.html', LoginHandler),
@@ -70,14 +79,28 @@ class CustomApplication(tornado.web.Application):
                 'startbootstrap-clean-blog'),
             'login_url': '/login.html',
             'cookie_secret': "61oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o",
-            'xsrf_cookies': True}
+            'xsrf_cookies': True,
+            'debug': debug,
+            'pycket': {
+                'engine': 'redis',
+                'storage': {
+                    'host': 'localhost',
+                    'port': 6379,
+                    'db_sessions': 10,
+                    'db_notifications': 11,
+                    'max_connections': 2**31,
+                }
+            }
+        }
         super(CustomApplication, self).__init__(handles, **settings)
 
 
 if __name__ == '__main__':
+    tornado.options.parse_command_line()
     # 实例化一个httpserver对象
-    http_server = tornado.httpserver.HTTPServer(CustomApplication())
+    http_server = tornado.httpserver.HTTPServer(CustomApplication(debug=options.debug))
     # 监听8888 套接字端口
-    http_server.listen(8888)
+    http_server.listen(options.port)
     # 启动事件循环
+    tornado.autoreload.start()
     tornado.ioloop.IOLoop.instance().start()
